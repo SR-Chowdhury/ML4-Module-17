da2e4a0cae70270d61efa8e1e0a4c403cc96aedb
Shihan Chowdhury <hackedgdown@gmail.com>
2023-01-28 00:02:03 +0000

    Module 17 done

---
+++ problem.js
@@ -0,0 +1,27 @@
+// Problem 1
+var totalAmount = 1000;
+var orangePrice = applePrice = 350;
+var expense = orangePrice + applePrice;
+var changeMoney = totalAmount - expense;
+console.log('\nProblem 1: \nShop Keeper will return: '+ changeMoney+'Tk');
+
+// Problem 2
+var mathMarks = 75.25;
+var biologyMarks = 65;
+var chemistryMarks = 35.45;
+var physicsMarks = 80;
+var banglaMarks = 99.50;
+var totalMarks = mathMarks + biologyMarks + chemistryMarks + physicsMarks + banglaMarks;
+var averageMarks = totalMarks / 5;
+console.log('\nProblem 2: \n Average Marks: '+ averageMarks.toFixed(2)); 
+
+// Problem 3
+var input1 = 'I am going to be';
+var input2 = 'an awesome web developer';
+var output = input1 + ' ' + input2;
+console.log('\nProblem 3:\nOutput: '+output+'\n');
+
+// Problem 4
+var x = 119;
+var reminderOfX = x % 5;
+console.log('\nProblem 4 [N>B> % is called modular operator]:\nReminder: '+reminderOfX+'\n');
